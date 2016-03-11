628e0861acd5eb918c94e0a16a307b6fe9827991
Rosemary <rosemarycadams@gmail.com>
2016-03-10 22:59:34 -0800

    comments

--- scripts/BuildEducationMap.R
+++ scripts/BuildEducationMap.R
@@ -1,23 +1,23 @@
 #Creates interactive choropleth map of education data
 
 # install.packages("plotly")
-require(plotly)
-require(dplyr)
-
-df <- read.csv("./data/EducationData.csv")
-
 #year options: even years between 1992 and 2010, parameter formatted as just the year
 
 EducationMap <- function(year_input) {
   require(plotly)
   require(dplyr)
 
-  df <- read.csv("./data/EducationData.csv")
+  educationData <- read.csv("./data/EducationData.csv")
   
+  #format the parameter to work with the colnames of the data frame
   year <- paste0("x", year_input)
   
   #test condition
   #year <- "x2010"
+  
+  #hover text
+  educationData$hover <- with(educationData, paste0(State, '<br>', 
+                              "Percent College Attendance: ", educationData[,year]))
 
   #give state boundaries a white border
   l <- list(color = toRGB("white"), width = 2)
@@ -30,8 +30,8 @@ EducationMap <- function(year_input) {
   )
   
   #builds map
-  plot_ly(df, z = df[,year], locations = code, type = 'choropleth', 
-          locationmode = 'USA-states', color = df[,year], colors = 'Purples',
+  plot_ly(educationData, z = educationData[,year], locations = code, text = hover, type = 'choropleth', 
+          locationmode = 'USA-states', color = educationData[,year], colors = 'Purples',
           marker = list(line = l), colorbar = list(title = " % College Attendance")) %>%
     layout(title = 'Percent of High School Graduates who Attend College', geo = g)
 }
